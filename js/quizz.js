dc0743ae13ff8c6db3fb18a347a142c8e4d67311
vitorregisr <vitorregisrr@gmail.com>
2019-07-01 11:29:31 -0300

    Merge carousels.js with quizz.js

--- js/quizz.js
+++ js/quizz.js
@@ -1,6 +1,32 @@
 (function () {
     'use strict';
 
+    // Start questions carousel //
+    let questionsCarousel = $(".owl-carousel.quizz__carousel");
+    questionsCarousel = questionsCarousel.owlCarousel({
+        loop: false,
+        margin: 30,
+        items: 1,
+        center: true,
+        dots: false,
+        nav: false,
+        navText: [
+            "<i class='owl-nav chevron-left'>", "<i class='owl-nav chevron-right'>"
+        ],
+        slideBy: "page",
+        dragEndSpeed: 700,
+        mouseDrag: false,
+        touchDrag: false,
+        pullDrage: false,
+        freeDrag: false,
+        smartSpeed: 60000,
+        animateOut: 'zoomOut',
+        animateIn: 'zoomIn',
+        startPosition: 0,
+    });
+    
+    window.dispatchEvent(new Event('resize'));
+
     ////**  Create Global State **////
     const quizz = {
         user: {
